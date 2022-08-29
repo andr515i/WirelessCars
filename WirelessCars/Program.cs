c3f23fb39fe1e4f69723029aa21e643be40f2445
andr515i <andr515i@zbc.dk>
2022-08-29 14:11:56 +0200

    added wireless car class, added properties, added methods, and made a little game loop to test functionality

--- WirelessCars/Program.cs
+++ WirelessCars/Program.cs
@@ -10,6 +10,60 @@ namespace WirelessCars
     {
         static void Main(string[] args)
         {
+            WirelessCar WC = new WirelessCar(); // WC stands for Wireless Car
+
+            Console.Write(" to start the car, please insert key by typing 1. type 0 to cancel\n");
+            int input = int.Parse(Console.ReadLine());
+            if (input == 1)  // temporary start of car, to make sure we only create one for now
+            {
+               
+
+                Console.WriteLine(" what color is your car?\n ");
+                WC.color = Console.ReadLine();
+
+                Console.WriteLine(" what brand is your car then? \n");
+                WC.name = Console.ReadLine(); 
+                WC.TurnOn();
+            }
+            else
+            {
+                // closes program to signal cancellation was successfull
+            }
+
+             
+            while (WC.isCarTurnedOn)    // saving this for later, basically a game loop to test various 
+            {
+                Console.WriteLine(" what action would you like to take?\n 1 to drive forward, 2 to recharge the car's internal battery, 3 to turn car off, 4 to buy a new car \n");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(" how many times would you like to drive forward? \n");
+                        int driveAmount = int.Parse(Console.ReadLine());
+                        WC.Drive(driveAmount);
+                        break;
+                    case 2:
+                        Console.WriteLine(" your car has been recharged");
+                        WC.Recharge();
+                        break;
+                    case 3:
+                        Console.WriteLine(" you successfully turned your car off");
+                        WC.TurnOff();
+                        break;
+                    case 4:
+                        WirelessCar WC2 = new WirelessCar();
+                        Console.WriteLine(" what color would you like your new car in?");
+                        WC2.color = Console.ReadLine();
+
+                        Console.WriteLine(" what brand would you like to purchase? we currently have a nice 0.05% discount on bugatti cars");
+                        WC2.name = Console.ReadLine();
+
+                        WC2.TurnOn();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
---
+++ WirelessCars/WirelessCar.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WirelessCars
+{
+    class WirelessCar
+    { //properties on the wireless cars
+        private string _color;  // color of the car
+        private int _batteryCharge = 100;  // battery out of box, always 100 so we just declare it now
+        private int _meterCounter;  // the total amount of meters driven on the current battery charge
+        private string _name;
+        public bool isCarTurnedOn = false; // true if car is turned on, false if car is turned off
+        
+        public int meterCounter { get => _meterCounter; set => _meterCounter = value; }
+        public string color { get => _color; set => _color = value; }
+        public int batteryCharge { get => _batteryCharge; set => _batteryCharge = value; }
+        public string name { get => _name; set => _name = value; }
+
+        public void TurnOn() // turns car on so car can function
+        {
+            isCarTurnedOn = true; // turn the car on
+            DisplayAllStatistics(); //display all information on car
+        }
+
+
+
+        public void TurnOff() // turns car off so car cannot function
+        {
+            // turns the car off
+            //display all information on car?
+            isCarTurnedOn = false; // turn the car off
+            DisplayAllStatistics();
+        }
+        public void Drive(int driveTimes)  // how many times we drive the car
+        {
+            if (this.isCarTurnedOn)
+            {
+                if (this._batteryCharge > 0) //we check outside the for loop to make sure we even have the battery to go
+                {
+                    // drives 20 meters forward, and drains battery in increments of 1(%)
+                    for (int i = 0; i < driveTimes; i++)
+                    {
+                        if (this._batteryCharge > 0) // check inside for battery left, to make sure that we dont go into negative 
+                        {
+                            DrainBattery();
+                            MeterCounterIncrease();
+                        }
+                        else
+                        {
+                            ResetCarStatistics();
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    ResetCarStatistics();
+                }
+            }
+            DisplayStatistics();
+        }
+        private void DrainBattery() // drains battery when driving
+        {
+
+            this._batteryCharge--; // we remove 1 % battery charge when driving once
+
+        }
+
+        private void MeterCounterIncrease() // increases meter counter when driving
+        {
+            this._meterCounter += 20; // here we increase the meter counter by 20
+
+        }
+        private void ResetCarStatistics() // here we reset the meters and battery to defualt values
+        {
+            Recharge(); 
+
+            Console.WriteLine(" You have driven so far that your battery has run out, and will be charging for the next fortnight to regain power");
+                
+        }
+        public void DisplayStatistics() // displays normal stats on car
+        {
+            Console.WriteLine(" --- current battery charge: " + this._batteryCharge + "%\ndistance until recharge is needed: " + (this.batteryCharge * 20) + " --- \n\n");
+        }
+        private void DisplayAllStatistics() // displays all stats on car
+        {
+            Console.WriteLine(" --- current battery charge: " + this._batteryCharge + "%\n" + "current meter count: " + this._meterCounter + "\nthe color of this car is: " + this._color + "\ncar's brand: " + this.name +" \ndistance until recharge is needed: " + (this.batteryCharge * 20) + " --- \n\n");
+        
+        }
+        public void Recharge()
+        {
+            this.meterCounter = 0;
+            this.batteryCharge = 100;
+
+        }
+
+
+    }
+
+}
